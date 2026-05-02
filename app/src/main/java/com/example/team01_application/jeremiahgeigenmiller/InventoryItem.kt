43f96ef8a2996d45958e00101dea4ad9ce2a6e64
Jeremiah Geigenmiller <jgeigenmiller@gmail.com>
2026-05-01 21:25:21 -0500

    Added User, InventoryItem, and ProfileViewModel classes

---
+++ app/src/main/java/com/example/team01_application/jeremiahgeigenmiller/InventoryItem.kt
@@ -0,0 +1,9 @@
+package com.example.team01_application.jeremiahgeigenmiller
+
+data class InventoryItem(
+    val id: String = "",
+    val name: String = "",
+    val type: String = "",
+    val description: String = "",
+    val quantity: Int = 1
+)
