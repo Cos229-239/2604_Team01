6d2f7f06ca91c48de42a89128ae52a5d58739893
Joseph A Davis <sethdavis1007@outlook.com>
2026-05-06 18:54:20 -0500

    Made Changes to the Character Creator
    Since we will eventually be adding in UI, I started adjusting the code for a UI setup in Android Studio.

--- app/src/main/java/Character_Creation.kt
+++ app/src/main/java/Character_Creation.kt
@@ -1,27 +1,19 @@
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class Character_Creation : ViewModel() {
+class Character_Creation(
+    private val race_Repo: RaceRepository,
+    private val class_Repo: Class_Repository,
+    private val item_Repo: Item_Repository
+) : ViewModel() {
 
-    // ------ Used to encapsulate the data for usage -----------
-    private val _character = MutableLiveData<Character>(Character(ch_Name = "New"))
-    val character: LiveData<Character> = _character
+    // ---- The Character that is being created ------
+    private val _character = MutableStateFlow( Character(ch_Name = null, ch_Race = null, ch_Class = null))
+    val character: StateFlow<Character> get() = _character.asStateFlow()
 
-    fun Set_Name(ch_Name: String)
-    { _character.value = _character.value?.copy(ch_Name = ch_Name)}
+    // ---- Ability Score rolling and adjustment handling -----
 
-    fun Set_Level(ch_Level: Int)
-    { _character.value = _character.value?.copy(ch_Level = ch_Level)}
 
-    fun Set_Race(ch_Race: Race)
-    { _character.value = _character.value?.copy(ch_Race = ch_Race) }
-
-    fun Set_Class(ch_Class: CharClass)
-    { _character.value = _character.value?.copy(ch_Class = ch_Class) }
-
-    fun Save_Character()
-    { viewModelScope.launch { }}
 }
