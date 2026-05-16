1744790887ed5a84d7425589b18888af7bc0b6c2
jacobeawalters <47542764+jacobeawalters@users.noreply.github.com>
2026-05-16 16:34:32 -0400

    added dragon and dwarf fragments

---
+++ app/src/main/java/com/example/team01_application/raceinfofragments/DragonFragment.kt
@@ -0,0 +1,37 @@
+package com.example.team01_application.raceinfofragments
+
+import android.os.Bundle
+import com.example.team01_application.R
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import model.RaceRepository
+
+
+class DragonFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(
+            R.layout.fragment_dragon,
+            container,
+            false
+        )
+
+        val raceRepository = RaceRepository()
+        val dragonRace = raceRepository.dragonBornRace
+
+        view.findViewById<TextView>(R.id.tvDragonName).text = dragonRace.name
+        view.findViewById<TextView>(R.id.tvDragonDescription).text = dragonRace.description
+        view.findViewById<TextView>(R.id.tvDragonBackground).text = dragonRace.background
+        view.findViewById<TextView>(R.id.tvDragonTraits).text = dragonRace.traits
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/team01_application/raceinfofragments/DwarfFragment.kt
@@ -0,0 +1,36 @@
+package com.example.team01_application.raceinfofragments
+
+import android.os.Bundle
+import com.example.team01_application.R
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import model.RaceRepository
+
+class DwarfFragment : Fragment(R.layout.fragment_dwarf) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(
+            R.layout.fragment_dwarf,
+            container,
+            false
+        )
+
+        val raceRepository = RaceRepository()
+        val dwarfRace = raceRepository.dwarfRace
+
+        view.findViewById<TextView>(R.id.tvDwarfName).text = dwarfRace.name
+        view.findViewById<TextView>(R.id.tvDwarfDescription).text = dwarfRace.description
+        view.findViewById<TextView>(R.id.tvDwarfBackground).text = dwarfRace.background
+        view.findViewById<TextView>(R.id.tvDwarfTraits).text = dwarfRace.traits
+
+        return view
+    }
+}
