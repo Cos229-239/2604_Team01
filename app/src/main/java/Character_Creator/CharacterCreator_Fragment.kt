48891eb1683752051be7c1609ab4eb1c23b0aa18
Joseph A Davis <sethdavis1007@outlook.com>
2026-05-19 19:53:23 -0500

    Started refining the Fragment for Character Creation
    Started the process of adding all the buttons and refining the UI in the Character Creation Fragment.

--- app/src/main/java/Character_Creator/CharacterCreator_Fragment.kt
+++ app/src/main/java/Character_Creator/CharacterCreator_Fragment.kt
@@ -8,22 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.team01_application.R
 
-class CharacterCreation_Fragment : Fragment() {
+class CharacterCreation_Fragment : Fragment( R.layout.fragment_character_creation) {
 
-    override fun onCreateView (
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View?
-    {
-        val view = inflater.inflate(
-            R.layout.fragment_character_creation,
-            container,
-            false
-        )
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    { super.onViewCreated(view, savedInstanceState) }
 
-        return view
-    }
 
 
 }
