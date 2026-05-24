8f8422c467502ab6af6045e4b930f09c8f4c65f6
jacobeawalters <47542764+jacobeawalters@users.noreply.github.com>
2026-05-23 20:00:43 -0400

    added fragment for race info selection

--- app/src/main/java/com/example/team01_application/raceinfofragments/RaceInfoMenuFragment.kt
+++ app/src/main/java/com/example/team01_application/raceinfofragments/RaceInfoMenuFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.team01_application.R
 
 
@@ -14,7 +15,56 @@ class RaceInfoMenuFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_race_info_menu, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_race_info_menu,
+            container,
+            false)
+
+
+        // fragment to display
+        val humanFragment = HumanFragment()
+        val dwarfFragment = DwarfFragment()
+        val elfFragment = ElfFragment()
+        val dragonFragment = DragonFragment()
+
+        val btnHumanFragment = view.findViewById<Button>(R.id.btnHumanFragment)
+        val btnDwarfFragment = view.findViewById<Button>(R.id.btnDwarfFragment)
+        val btnElfFragment = view.findViewById<Button>(R.id.btnElfFragment)
+        val btnDragonFragment = view.findViewById<Button>(R.id.btnDragonFragment)
+
+
+        // on click listeners
+        // using my own frame layout to swap fragments, when we have a main activity I will swap
+
+        btnHumanFragment.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flRaceMenuFragment, humanFragment)
+                commit()
+            }
+        }
+
+        btnDwarfFragment.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flRaceMenuFragment, dwarfFragment)
+                commit()
+            }
+        }
+
+        btnElfFragment.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flRaceMenuFragment, elfFragment)
+                commit()
+            }
+        }
+
+        btnDragonFragment.setOnClickListener {
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flRaceMenuFragment, dragonFragment)
+                commit()
+            }
+        }
+
+        return view
     }
+
 }
